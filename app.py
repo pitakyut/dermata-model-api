e2af2097582435d702c4dd43304ab479816fd705
Pitakyut <puspitasaria012@gmail.com>
2024-12-12 03:21:54 +0700

    fix: missing app.run

--- app.py
+++ app.py
@@ -78,3 +78,6 @@ def predict_image():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
